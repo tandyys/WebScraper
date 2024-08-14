6d58b5b6b0d1521da31bd39c2d9a716c4c114a30
Tandy Sabana <tansabana12@gmail.com>
2024-08-14 20:59:34 +0700

    Prettier
    Prettier

--- main.go
+++ main.go
@@ -4,27 +4,27 @@ import (
 	"fmt"
 	"log"
 	"strings"
-	
+
 	"github.com/gocolly/colly/v2"
 )
 
 func main() {
-	collector:=colly.NewCollector()
+	collector := colly.NewCollector()
 
-	url:="https://nasional.kompas.com/"
+	url := "https://nasional.kompas.com/"
 
 	collector.OnHTML(".articleItem", func(h *colly.HTMLElement) {
-		title:=h.ChildText("h2.articleTitle")
-		date:=h.ChildText("div.articlePost-date")
-		link:=h.ChildAttr("a", "href")
-		link=strings.TrimSpace(link)
-		date=strings.TrimSpace(date)
-		title=strings.TrimSpace(title)
-		
+		title := h.ChildText("h2.articleTitle")
+		date := h.ChildText("div.articlePost-date")
+		link := h.ChildAttr("a", "href")
+		link = strings.TrimSpace(link)
+		date = strings.TrimSpace(date)
+		title = strings.TrimSpace(title)
+
 		fmt.Printf("Article title : %s\nDate : %s\nHere's the link to the article :\n%s\n\n", title, date, link)
 	})
 
-	err:=collector.Visit(url)
+	err := collector.Visit(url)
 	if err != nil {
 		log.Fatal(err)
 	}
